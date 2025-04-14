b1d7f7858b4e4d7d8e13555857760d203b5f7b7a
Sambhavi Chakraborty <samsam2112chak@gmail.com>
2025-04-14 16:26:58 +0530

    Add files via upload

---
+++ P4Q1sumav.cpp
@@ -0,0 +1,26 @@
+//Write a program for sum and average of two numbers.
+#include <iostream>
+using namespace std;
+class sum
+{
+    int a,b;
+    public:
+    void getdata()
+    {
+        cout<<"Enter first value: ";
+        cin>>a;
+        cout<<"Enter second value: ";
+        cin>>b;
+    }
+    inline void display()
+    {
+        cout<<"Sum: "<<a+b<<endl;
+        cout<<"Average: "<<(a+b)/2<<endl;
+    }
+};
+int main()
+{
+    sum s;
+    s.getdata();
+    s.display();
+}
---
+++ P4Q2sample.cpp
@@ -0,0 +1,28 @@
+//non-member function sum() has to print the sum of a and b.
+#include <iostream>
+using namespace std;
+class sample
+{
+    int a,b;
+    public:
+    void setval()
+    {
+        cout<<"Enter the value: ";
+        cin>>a;
+        cout<<"Enter the second value: ";
+        cin>>b;
+    }
+    friend void sum(sample p);
+};
+void sum(sample p)
+{
+    int s= p.a+p.b;
+    cout<<"Sum: "<<s;
+}
+int main()
+{
+    sample p;
+    p.setval();
+    //q.setval();
+    sum(p);
+}
